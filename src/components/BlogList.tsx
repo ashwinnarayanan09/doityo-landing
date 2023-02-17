6ba85993b433c223bb64a30ea156ebc3d9f1a541
ashwinnarayanan09 <ashwinnarayanan09@gmail.com>
2023-02-17 14:08:24 +0100

    blogs

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,65 +1,80 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "flowbite-react";
 import { Link } from "react-router-dom";
-
+import { fetchBlogs, filterBlogs } from "../services/blog.service";
 import { Blog } from "../models/Blog";
 
 const BlogList = () => {
-  const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [blogs, setFilteredBlogs] = useState<Blog[]>([]);
   // fetch data
-  const baseUrl =
-    "https://www.googleapis.com/blogger/v3/blogs/3115211964733177924/posts?key=AIzaSyCYtJeewAy6JhfVfOjW_J7tCZ5-6Q4VktE";
-
-  const fetchBlogs = async () => {
-    const response = await (await fetch(baseUrl)).json();
-
-    const data: Blog[] = [];
-    const parser = new DOMParser();
-    response.valueOf()["items"].forEach((element) => {
-      const html = parser.parseFromString(element["content"], "text/html");
-      const img = html.querySelector("img");
-      const url = img ? img.src : "";
-
-      let blog = new Blog(
-        element["title"].toLowerCase().replaceAll(" ", "-"),
-        element["title"],
-        element["content"],
-        url,
-        element["labels"][0],
-        element["status"],
-        element["author"]
-      );
-      data.push(blog);
-    });
-    setBlogs(data);
-  };
 
   useEffect(() => {
-    fetchBlogs();
+    fetchBlogs().then((res) => {
+      setFilteredBlogs(res);
+    });
   }, []);
+
+  const buttons = [
+    {
+      name: "All",
+      value: "all",
+    },
+  ];
+
+  blogs.map((blog) => {
+    buttons.push({ name: blog.area, value: blog.area });
+    console.log(buttons);
+  });
+
+  function filterBlogs(area) {
+    return blogs.filter((blog) => blog.area == area);
+  }
+
+  function handleBlogs(e) {
+    let area = e.target.value;
+    area !== "all"
+      ? setFilteredBlogs(filterBlogs(area))
+      : fetchBlogs().then((res) => setFilteredBlogs(res));
+  }
+
   return (
-    <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-      {blogs.map((item, i) => (
-        <div className="max-w-sm">
-          <Card imgSrc={item.image} style={Container}>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              <Link
-                to={`/blogs/${item.id}`}
-                className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                aria-current="page"
+    <>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        {buttons &&
+          buttons.map((type, index) => (
+            <>
+              <button
+                key={index}
+                value={type.value}
+                onClick={handleBlogs}
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
               >
-                {item.title}
-              </Link>
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-                {item.area}
-              </span>
-            </p>
-          </Card>
-        </div>
-      ))}
-    </div>
+                {type.name}
+              </button>
+            </>
+          ))}
+        {blogs.map((item, i) => (
+          <div className="max-w-sm">
+            <Card imgSrc={item.image} style={Container}>
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <Link
+                  to={`/blogs/${item.id}`}
+                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  {item.title}
+                </Link>
+              </h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                  {item.area}
+                </span>
+              </p>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,9 +1,41 @@
-export function getBlogs() {
-  const pokemonList = [];
-  return pokemonList;
+import React, { useState, useEffect } from "react";
+import { Card } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+import { Blog } from "../models/Blog";
+
+const baseUrl =
+  "https://www.googleapis.com/blogger/v3/blogs/3115211964733177924/posts?key=AIzaSyCYtJeewAy6JhfVfOjW_J7tCZ5-6Q4VktE";
+
+const fetchBlogs = async () => {
+  const response = await (await fetch(baseUrl)).json();
+
+  const data: Blog[] = [];
+  const parser = new DOMParser();
+  response.valueOf()["items"].forEach((element) => {
+    const html = parser.parseFromString(element["content"], "text/html");
+    const img = html.querySelector("img");
+    const url = img ? img.src : "";
+
+    let blog = new Blog(
+      element["title"].toLowerCase().replaceAll(" ", "-"),
+      element["title"],
+      element["content"],
+      url,
+      element["labels"][0],
+      element["status"],
+      element["author"]
+    );
+    data.push(blog);
+  });
+  return data;
+};
+
+function filterBlogs(area) {
+  console.log(area);
+  //let filtredBlogs = Blog[]
+  //fetchBlogs().then(res=> filtredBlogs = res);
+  return fetchBlogs();
 }
 
-export function filterBlogs(area) {
-  //let filtredBlogs = getBlogs().filter((type) => === area);
-  //return filtredBlogs;
-}
+export { fetchBlogs, filterBlogs };
